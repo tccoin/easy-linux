bd60ec7b608c96f6ec5190c02c62ce00799e529b
tccoin <1774241372@qq.com>
2019-12-18 15:33:03 +0800

    update zsh setup

--- zsh.sh
+++ zsh.sh
@@ -10,3 +10,5 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 echo "source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+cp conf/.zshrc ~/.zshrc
+cp conf/.p10k.zsh ~/.p10k.zsh
