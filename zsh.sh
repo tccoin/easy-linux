4115cd87f0260664f87270774e4562edb6174b22
tccoin <1774241372@qq.com>
2019-12-18 15:30:14 +0800

    use var

--- zsh.sh
+++ zsh.sh
@@ -7,4 +7,6 @@ cd fonts
 cd ..
 rm -rf fonts
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-https://github.com/romkatv/powerlevel10k.git
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+echo "source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
